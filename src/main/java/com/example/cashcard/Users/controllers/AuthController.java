a8769f82a55cd23b22338e45d8ed4541754bca74
juan <juanchuu.jfpc@gmail.com>
2024-02-01 15:09:38 +0100

    feat: added payload for login and Signup request

--- src/main/java/com/example/cashcard/Users/controllers/AuthController.java
+++ src/main/java/com/example/cashcard/Users/controllers/AuthController.java
@@ -1,4 +1,43 @@
 package com.example.cashcard.Users.controllers;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import com.example.cashcard.Users.Login.Payload.LoginRequest;
+import com.example.cashcard.Users.Repositories.RoleRepository;
+import com.example.cashcard.Users.Repositories.UserRepository;
+import com.example.cashcard.Users.security.jwt.JwtUtils;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/api/auth")
 public class AuthController {
+    @Autowired
+    AuthenticationManager authenticationManager;
+    @Autowired
+    UserRepository userRepository;
+    @Autowired
+    RoleRepository roleRepository;
+
+    @Autowired
+    PasswordEncoder encoder;
+
+    @Autowired
+    JwtUtils jwtUtils;
+
+    @PostMapping("/signin")
+    private ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest){
+        return null;
+    }
+
+
 }
